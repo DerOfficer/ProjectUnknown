7e12ef1b2a943e2870b5d64bfd28bb2a49fab971
mutterficker69 <torpia132@gmx.de>
2016-12-14 12:29:29 +0100

    kiuo

--- src/Model/VolumeManager.java
+++ src/Model/VolumeManager.java
@@ -4,4 +4,24 @@ package Model;
  * Created by 204g03 on 12.12.2016.
  */
 public class VolumeManager {
+    double volume;
+
+    public VolumeManager(){
+        volume = 1;
+    }
+
+    public void increase(){
+        if(checkBounds()) {
+            volume = volume + 0.01;
+        }
+    }
+    public void decrease(){
+        if(checkBounds()) {
+            volume = volume - 0.01;
+        }
+    }
+
+    public boolean checkBounds(){
+        return (volume > 0 && volume < 1);
+    }
 }
