a55fe7717b882e6cf19f93bd2ba6ade3b138e5fd
amasso99 <khouri.oussama@gmx.de>
2016-12-17 21:42:58 +0100

    added SpriteSheetHandler

---
+++ src/Model/SpriteSheetHandler.java
@@ -0,0 +1,35 @@
+package Model;
+
+import java.awt.image.BufferedImage;
+
+/**
+ * Created by Amasso on 17.12.2016.
+ */
+
+public class SpriteSheetHandler {
+    private BufferedImage image;
+    private BufferedImage[]sprites;
+    private int[]gaps;
+
+    public SpriteSheetHandler(int[]gaps, BufferedImage image){
+        this.gaps = gaps;
+        this.image = image;
+
+        subSpriteImage();
+    }
+
+    private void subSpriteImage() {
+        if (image != null){
+            sprites = new BufferedImage[gaps.length+1];
+            int temp = 0;
+            for (int i = 1; i < sprites.length+1; i++){
+                temp = temp + gaps[i-1];
+                sprites[i] = image.getSubimage(temp,0,gaps[i], image.getHeight());
+            }
+        }
+    }
+
+    public BufferedImage[] getSprites(){
+        return sprites;
+    }
+}
