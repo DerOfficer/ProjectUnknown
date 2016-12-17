39d0fbfe2e9fb44d794faa85511e7747b4cde77f
xXraptrXx <max@ausbuettels.de>
2016-12-17 17:19:42 +0100

    l

--- src/Model/LevelSelect.java
+++ src/Model/LevelSelect.java
@@ -13,15 +13,25 @@ import java.awt.event.KeyEvent;
  */
 public class LevelSelect extends DrawingPanel {
 
+
+    private Image earth = Toolkit.getDefaultToolkit().getImage("earth.png");
+    private Image jupiter = Toolkit.getDefaultToolkit().getImage("jupiter.png");
+    private Image mars = Toolkit.getDefaultToolkit().getImage("mars.png");
+    private Image mercury = Toolkit.getDefaultToolkit().getImage("mercury.png");
+    private Image neptune = Toolkit.getDefaultToolkit().getImage("neptune.png");
+    private Image saturn = Toolkit.getDefaultToolkit().getImage("saturn.png");
+    private Image uranus = Toolkit.getDefaultToolkit().getImage("uranus.png");
+    private Image venus = Toolkit.getDefaultToolkit().getImage("venus.png");
+
     private LevelSelectBackground sunSystem = new LevelSelectBackground();
-    private Planet mercury = new Planet("mercury",200,200);
-    private Planet venus = new Planet("venus",300,200);
-    private Planet earth = new Planet("earth",400,200);
-    private Planet mars = new Planet("mars",500,200);
-    private Planet jupiter = new Planet("jupiter",600,200);
-    private Planet saturn = new Planet("saturn",700,200);
-    private Planet uranus = new Planet("uranus",800,200);
-    private Planet neptune = new Planet("neptune",900,200);
+    private Planet planetMercury = new Planet(mercury,200,200);
+    private Planet planetVenus = new Planet(venus,300,200);
+    private Planet planetEarth = new Planet(earth,400,200);
+    private Planet planetMars = new Planet(mars,500,200);
+    private Planet planetJupiter = new Planet(jupiter,600,200);
+    private Planet planetSaturn = new Planet(saturn,700,200);
+    private Planet planetUranus = new Planet(uranus,800,200);
+    private Planet planetNeptune = new Planet(neptune,900,200);
 
     private Model.Button back = new Model.Button(200,800,100,50,"← Back");
 
@@ -29,45 +39,46 @@ public class LevelSelect extends DrawingPanel {
 
     public LevelSelect(ProjectUnknownProperties properties){
         super(properties);
-        addObject(mercury);
-        addObject(venus);
-        addObject(earth);
-        addObject(mars);
-        addObject(jupiter);
-        addObject(saturn);
-        addObject(uranus);
-        addObject(neptune);
+        addObject(planetMercury);
+        addObject(planetVenus);
+        addObject(planetEarth);
+        addObject(planetMars);
+        addObject(planetJupiter);
+        addObject(planetSaturn);
+        addObject(planetUranus);
+        addObject(planetNeptune);
         addObject(back);
 
 
-        mercury.addEventHandler(IEventInteractableObject.EventType.MOUSE_RELEASED, (event) ->
+        planetMercury.addEventHandler(IEventInteractableObject.EventType.MOUSE_RELEASED, (event) ->
                 System.out.println("mercury")
         );
-        venus.addEventHandler(IEventInteractableObject.EventType.MOUSE_RELEASED, (event) ->
+        planetVenus.addEventHandler(IEventInteractableObject.EventType.MOUSE_RELEASED, (event) ->
                 System.out.println("venus")
         );
-        earth.addEventHandler(IEventInteractableObject.EventType.MOUSE_RELEASED, (event) ->
+        planetEarth.addEventHandler(IEventInteractableObject.EventType.MOUSE_RELEASED, (event) ->
                 System.out.println("earth")
         );
-        mars.addEventHandler(IEventInteractableObject.EventType.MOUSE_RELEASED, (event) ->
+        planetMars.addEventHandler(IEventInteractableObject.EventType.MOUSE_RELEASED, (event) ->
                 System.out.println("mars")
         );
-        jupiter.addEventHandler(IEventInteractableObject.EventType.MOUSE_RELEASED, (event) ->
+        planetJupiter.addEventHandler(IEventInteractableObject.EventType.MOUSE_RELEASED, (event) ->
                 System.out.println("jupiter")
         );
-        saturn.addEventHandler(IEventInteractableObject.EventType.MOUSE_RELEASED, (event) ->
+        planetSaturn.addEventHandler(IEventInteractableObject.EventType.MOUSE_RELEASED, (event) ->
                 System.out.println("saturn")
         );
-        uranus.addEventHandler(IEventInteractableObject.EventType.MOUSE_RELEASED, (event) ->
+        planetUranus.addEventHandler(IEventInteractableObject.EventType.MOUSE_RELEASED, (event) ->
                 System.out.println("uranus")
         );
-        neptune.addEventHandler(IEventInteractableObject.EventType.MOUSE_RELEASED, (event) ->
+        planetNeptune.addEventHandler(IEventInteractableObject.EventType.MOUSE_RELEASED, (event) ->
                 System.out.println("neptune")
         );
 
         back.addEventHandler(IEventInteractableObject.EventType.MOUSE_RELEASED, (event) -> {
             properties.getFrame().setDrawingPanel(properties.getFrame().getStart());
         });
+
         back.addEventHandler(IEventInteractableObject.EventType.KEY_RELEASED, (event) -> {
             if(event.getSrcKey() == KeyEvent.VK_ESCAPE)
                 properties.getFrame().setDrawingPanel(properties.getFrame().getStart());
--- src/Model/Planet.java
+++ src/Model/Planet.java
@@ -11,25 +11,18 @@ import java.awt.geom.Ellipse2D;
  */
 public class Planet extends AbstractEventInteractionObject {
 
-    private String name;
+    private Image image;
     private int x;
     private int y;
 
-    private Image earth = Toolkit.getDefaultToolkit().getImage("earth.png");
-    private Image jupiter = Toolkit.getDefaultToolkit().getImage("jupiter.png");
-    private Image mars = Toolkit.getDefaultToolkit().getImage("mars.png");
-    private Image mercury = Toolkit.getDefaultToolkit().getImage("mercury.png");
-    private Image neptune = Toolkit.getDefaultToolkit().getImage("neptune.png");
-    private Image saturn = Toolkit.getDefaultToolkit().getImage("saturn.png");
-    private Image uranus = Toolkit.getDefaultToolkit().getImage("uranus.png");
-    private Image venus = Toolkit.getDefaultToolkit().getImage("venus.png");
+
 
 
 
     private ICanvas canvas;
 
-    public Planet(String name,int x, int y){
-        this.name = name;
+    public Planet(Image image,int x, int y){
+        this.image = image;
         this.x = x;
         this.y = y;
 
@@ -38,30 +31,7 @@ public class Planet extends AbstractEventInteractionObject {
     @Override
     public void draw() {
         Graphics2D g2d = canvas.getPencil();
-        if (name.equals("earth")){
-            g2d.drawImage(earth,x,y,null);
-        }
-        if (name.equals("jupiter")){
-            g2d.drawImage(jupiter,x,y,null);
-        }
-        if (name.equals("mars")){
-            g2d.drawImage(mars,x,y,null);
-        }
-        if (name.equals("mercury")){
-            g2d.drawImage(mercury,x,y,null);
-        }
-        if (name.equals("neptune")){
-            g2d.drawImage(neptune,x,y,null);
-        }
-        if (name.equals("saturn")){
-            g2d.drawImage(saturn,x,y,null);
-        }
-        if (name.equals("uranus")){
-            g2d.drawImage(uranus,x,y,null);
-        }
-        if (name.equals("venus")){
-            g2d.drawImage(venus,x,y,null);
-        }
+            g2d.drawImage(image,x,y,null);
     }
 
     @Override
