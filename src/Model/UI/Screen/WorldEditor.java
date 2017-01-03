6e2fe2251c9bb621a3d0fae27ba4d8c09b1f8291
amasso99 <khouri.oussama@gmx.de>
2017-01-03 13:20:26 +0100

    UPDATE

--- src/Model/UI/Screen/WorldEditor.java
+++ src/Model/UI/Screen/WorldEditor.java
@@ -11,12 +11,14 @@ import java.awt.event.KeyListener;
  * Created by Amasso on 03.01.2017.
  */
 public class WorldEditor extends DrawingPanel implements KeyListener {
-    private int camX,camY;
+    private int camX,camY,realX,realY;
 
     public WorldEditor(ProjectUnknownProperties properties) {
         super(properties);
         camX = 0;
         camY = 0;
+        realX = 0;
+        realY = 0;
 
     }
 
@@ -24,39 +26,40 @@ public class WorldEditor extends DrawingPanel implements KeyListener {
     public void paintComponent(Graphics g){
         g.setColor(Color.white);
         g.translate(camX,camY);
-            makeGrid(g);
-        g.translate(-camX,-camY);
-    }
+            for (int i = 0; i < (int)(screenHeight/50)+1; i++) {
+                g.drawLine(realX , realY+i*50 , realX+screenWidth, realY+i*50);
+            }
+            for (int i = 0; i < (int)(screenWidth/50)+1; i++) {
+                g.drawLine(realX+i*50 , realY , realX+i*50 ,realY+screenHeight);
+            }
 
-    private void makeGrid(Graphics g) {
-        for (int i = 0; i < (int)(screenHeight/50); i++) {
-            g.drawLine(camX , camY+i*50 , camX+screenWidth, camY+i*50);
-        }
-        for (int i = 0; i < (int)(screenWidth/50); i++) {
-            g.drawLine(camX+i*50 , camY , camX+i*50 ,camY+screenHeight);
-        }
     }
 
     @Override
     public void keyPressed(KeyEvent e) {
-
         int key = e.getKeyCode();
 
         if (key == KeyEvent.VK_LEFT) {
             camX = camX + 50;
+            realX = realX -50;
         }
 
         if (key == KeyEvent.VK_RIGHT) {
             camX = camX - 50;
+            realX = realX + 50;
         }
 
         if (key == KeyEvent.VK_UP) {
-            camY = camY - 50;
+            camY = camY + 50;
+            realY = realY - 50;
         }
 
         if (key == KeyEvent.VK_DOWN) {
-            camY = camY + 50;
+            camY = camY - 50;
+            realY = realY + 50;
         }
+
+
     }
 
 
