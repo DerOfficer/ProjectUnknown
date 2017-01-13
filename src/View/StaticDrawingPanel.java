0cad4783c6e757e42b5567710a95916a3833edba
stardust1971 <roy.2000@gmx.de>
2017-01-13 20:12:32 +0100

    removed left-over debugging code

--- src/View/StaticDrawingPanel.java
+++ src/View/StaticDrawingPanel.java
@@ -5,7 +5,6 @@ import Model.Abstraction.IDrawableObject;
 
 import java.awt.*;
 import java.awt.event.ActionEvent;
-import java.util.ConcurrentModificationException;
 
 /**
  * Created by jardu on 12/19/2016.
@@ -44,14 +43,8 @@ public class StaticDrawingPanel extends DrawingPanel {
         lastLoop = System.nanoTime();
         int dt = (int) ((elapsedTime / 1000000L)+0.5);
         if ( dt == 0 ) dt = 1;
-        IDrawableObject lastDrawableObject = null;
-        try {
-            for (IDrawableObject drawableObject : drawableObjects) {
-                drawableObject.update((double) dt / 1000);
-                lastDrawableObject = drawableObject;
-            }
-        }catch (ConcurrentModificationException e){
-            e.printStackTrace();
+        for (IDrawableObject drawableObject : drawableObjects) {
+            drawableObject.update((double) dt / 1000);
         }
     }
 }
